f303bdec6c3317287409bdd4d81f009457f1b799
KevinXuxuxu <kevin.xu.fangzhou@gmail.com>
2022-08-13 19:57:00 -0700

    use goroutine to handle concurrent requests

--- app/server.go
+++ app/server.go
@@ -7,32 +7,36 @@ import (
 	"os"
 )
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
-	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+func handleRequest(conn net.Conn) {
 	defer conn.Close()
 	for {
 		if _, err := conn.Read([]byte{}); err != nil {
 			fmt.Println("Error reading data from connection: ", err.Error())
 			os.Exit(1)
 		}
-		_, err = conn.Write([]byte("+PONG\r\n"))
+		// if _, err := conn.Write([]byte("+PONG\r\n")); err != nil {
+		// 	fmt.Println("Error sending data to connection: ", err.Error())
+		// 	os.Exit(1)
+		// }
+		conn.Write([]byte("+PONG\r\n"))
+	}
+}
+
+func main() {
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	fmt.Println("Logs from your program will appear here!")
+
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+	for {
+		conn, err := l.Accept()
 		if err != nil {
-			fmt.Println("Error sending data to connection: ", err.Error())
+			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
+		go handleRequest(conn)
 	}
 }
